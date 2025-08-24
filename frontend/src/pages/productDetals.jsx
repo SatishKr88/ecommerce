54c83065a6a8a264982d67cd6a4c0d643a1e6a0f
Satish Kumar <satishkr051202@gmail.com>
2025-08-24 14:43:17 +0530

    satish

--- frontend/src/pages/productDetals.jsx
+++ frontend/src/pages/productDetals.jsx
@@ -11,27 +11,29 @@ const ProductDetail = () => {
 
   return (
     <>
-      <div className='w-full h-full absolute flex mt-25'>
-        <div className='fixed'>
-          <img
-            src={product.image}
-            className="w-80 h-80 object-contain "
-          />
-          <button
-            className="border border-[#2874f0] text-[#2874f0] px-8 py-1 rounded text-sm hover:bg-[#f02828] hover:text-white transition ml-8 mt-2">
-            <i className="fa-solid fa-cart-shopping mr-2"></i> Add
-          </button>
+      <div className='w-full h-full flex '>
+        <div className='mt-25  mb-100'>
+          <div className='absolute'>
+            <img
+              src={product.image}
+              className="w-80 h-80 object-contain "
+            />
+            <button
+              className="border border-[#2874f0] text-[#2874f0] px-8 py-1 rounded text-sm hover:bg-[#f02828] hover:text-white transition ml-8 mt-2">
+              <i className="fa-solid fa-cart-shopping mr-2"></i> Add
+            </button>
 
-          <button
-            className="border border-[#2874f0] text-[#2874f0] px-8 py-1 rounded text-sm hover:bg-[#2874f0] hover:text-white transition ml-7 mt-2">
-            <i className="fa-solid fa-bag-shopping mr-2"></i> Buy
-          </button>
-        </div>
+            <button
+              className="border border-[#2874f0] text-[#2874f0] px-8 py-1 rounded text-sm hover:bg-[#2874f0] hover:text-white transition ml-7 mt-2">
+              <i className="fa-solid fa-bag-shopping mr-2"></i> Buy
+            </button>
+          </div>
 
-        <div className=' absolute left-[30%] ' >
-          <h1 className='font-bold'>{product.title}</h1>
-          <p>{product.description}</p>
-          <p>Price: ${product.price}</p>
+          <div className=' absolute left-[30%] ' >
+            <h1 className='font-bold'>{product.title}</h1>
+            <p>{product.description}</p>
+            <p>₹{product.price}</p>
+          </div>
         </div>
 
       </div>
@@ -45,7 +47,7 @@ function ProductDetails() {
     <>
       <Navbar />
       <ProductDetail />
-      {/* <Footer/> */}
+      <Footer />
     </>
   )
 
