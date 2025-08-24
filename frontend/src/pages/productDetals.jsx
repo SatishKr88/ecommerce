83c81fa9ec872b77122c04210ad92bc640cedf99
Satish Kumar <satishkr051202@gmail.com>
2025-08-24 10:01:50 +0530

    satish

--- frontend/src/pages/productDetals.jsx
+++ frontend/src/pages/productDetals.jsx
@@ -4,11 +4,15 @@ const ProductDetail = () => {
   const product = location.state?.product;
 
   if (!product) {
-    return <div>Product details not found.</div>; 
+    return <div>Product details not found.</div>;
   }
 
   return (
     <div>
+      <img
+        src={product.image}
+        className="w-full h-48 object-contain mx-auto mt-5"
+      />
       <h1>{product.title}</h1>
       <p>{product.description}</p>
       <p>Price: ${product.price}</p>
